4e0248308765ded454bbd2c227a6572f40707893
Soroush M Pour <soroushmpour@gmail.com>
2016-07-01 04:03:22 +0430

    fixed path file bug

--- funcs.py
+++ funcs.py
@@ -1,5 +1,5 @@
-def create_mem(id,name,last,age,classname,**subjects):
-    file_creat_path = open ('/Users/Soroush/Desktop/'+id+".mi",'w')
+def create_mem(path,id,name,last,age,classname,**subjects):
+    file_creat_path = open (path+id+".mi",'w')
     file_creat_path.writelines(id+"\n")
     file_creat_path.writelines(name+"\n")
     file_creat_path.writelines(last+"\n")
@@ -7,7 +7,7 @@ def create_mem(id,name,last,age,classname,**subjects):
     file_creat_path.writelines(classname+"\n")
     file_creat_path.writelines(subjects)
     file_creat_path.close()
-def load_mem(id):
-    file_load_path = open ('/Users/Soroush/Desktop/'+id+".mi",'r')
+def load_mem(path,id):
+    file_load_path = open (path+id+".mi",'r')
     load_read_lines = file_load_path.readlines()
     print(load_read_lines)
--- mainpage.py
+++ mainpage.py
@@ -5,6 +5,7 @@ while True:
     if command_first=="exit":
         break
     elif command_first == "create":
+      input_path=input("Enter the path : ")
       input_create_id=input("Enter members id : ")
       input_create_name=input("Enter name :")
       input_create_last=input("Enter last name :")
@@ -12,10 +13,11 @@ while True:
       input_create_classname=input("Enter class name :")
       input_create_subjectsnoneprocc=input("Enter subjects name and score :")
       input_create_subjects=ast.literal_eval(input_create_subjectsnoneprocc)
-      funcs.create_mem(input_create_id,input_create_name, input_create_last, input_create_age, input_create_classname, **input_create_subjects)
+      funcs.create_mem(input_path,input_create_id,input_create_name, input_create_last, input_create_age, input_create_classname, **input_create_subjects)
     elif command_first == "load":
+      input_path=input("Enter the path : ")
       input_load_id=input("Enter member id : ")
-      funcs.load_mem(input_load_id)
+      funcs.load_mem(input_path,input_load_id)
 
     else:
         print("Enter right command !")
